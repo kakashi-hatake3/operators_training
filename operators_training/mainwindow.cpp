28296ad64fd092022b493f9570eba2c03f41f88e
arqwore <60177171+kakashi-hatake3@users.noreply.github.com>
2024-04-29 00:02:10 +0300

    almost ready

--- operators_training/mainwindow.cpp
+++ operators_training/mainwindow.cpp
@@ -19,10 +19,22 @@
 #include <QTimer>
 #include <QDir>
 #include <QCoreApplication>
+#include <QHeaderView>
+
+// исправить размер таблицы, исправить чтобы в textEdit не хавалась последняя часть при быстром написании при повторе тренировки
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    // Установка размеров окна
+    setFixedSize(900, 500);
+    // Set table stretch and resize mode
+//    tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+//    tableWidget->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    // Установка названия программы
+    setWindowTitle("Тренажер");
 
     // Создание элементов управления
     testSelector = new QComboBox(this);
@@ -30,20 +42,29 @@ MainWindow::MainWindow(QWidget *parent)
     testSelector->addItem("Test 2");
     testSelector->addItem("Test 3");
 
-    button = new QPushButton("Button", this);
+    button1 = new QPushButton("Button", this);
+    button2 = new QPushButton("Button", this);
     lineEdit = new QLineEdit(this);
     comboBox = new QComboBox(this);
-    radioButton = new QRadioButton("Option", this);
-    tableWidget = new QTableWidget(3, 3, this);
+    radioButton1 = new QRadioButton("Option", this);
+    radioButton2 = new QRadioButton("Option", this);
+    radioButton3 = new QRadioButton("Option", this);
+    tableWidget = new QTableWidget(3, 2, this);
 
     // Размещение элементов управления
     QWidget *centralWidget = new QWidget(this);
     QVBoxLayout *layout = new QVBoxLayout(centralWidget);
     layout->addWidget(testSelector);
-    layout->addWidget(button);
-    layout->addWidget(lineEdit);
     layout->addWidget(comboBox);
-    layout->addWidget(radioButton);
+
+    layout->addWidget(button1);
+    layout->addWidget(button2);
+
+    layout->addWidget(lineEdit);
+    layout->addWidget(radioButton1);
+    layout->addWidget(radioButton2);
+    layout->addWidget(radioButton3);
+
     layout->addWidget(tableWidget);
     setCentralWidget(centralWidget);
 
@@ -53,14 +74,21 @@ MainWindow::MainWindow(QWidget *parent)
     finishAction = menu->addAction("Завершить тренировку");
     checkAction = menu->addAction("Проверить результат тренировки");
     speedAction = menu->addAction("Изменить скорость воспроизведения");
+    // Create exit action
+    exitAction = menu->addAction("Выход");
+    connect(exitAction, &QAction::triggered, this, &QMainWindow::close);
 
     // Подключение слотов к сигналам
     connect(startAction, &QAction::triggered, this, &MainWindow::startTraining);
     connect(finishAction, &QAction::triggered, this, &MainWindow::finishTraining);
     connect(checkAction, &QAction::triggered, this, &MainWindow::checkTrainingResult);
 
-    connect(button, &QPushButton::clicked, [this]() {
-        saveAction("Button clicked");
+    connect(button1, &QPushButton::clicked, [this]() {
+        saveAction("Button1 clicked");
+    });
+
+    connect(button2, &QPushButton::clicked, [this]() {
+        saveAction("Button2 clicked");
     });
 
     connect(lineEdit, &QLineEdit::textChanged, [this](const QString &text) {
@@ -71,18 +99,28 @@ MainWindow::MainWindow(QWidget *parent)
         saveAction("ComboBox index changed: " + QString::number(index));
     });
 
-    connect(radioButton, &QRadioButton::toggled, [this](bool checked) {
-        saveAction("RadioButton toggled: " + QString::number(checked));
+    connect(radioButton1, &QRadioButton::toggled, [this](bool checked) {
+        saveAction("RadioButton1 toggled: " + QString::number(checked));
+    });
+
+    connect(radioButton2, &QRadioButton::toggled, [this](bool checked) {
+        saveAction("RadioButton2 toggled: " + QString::number(checked));
+    });
+
+    connect(radioButton3, &QRadioButton::toggled, [this](bool checked) {
+        saveAction("RadioButton3 toggled: " + QString::number(checked));
     });
 
     connect(tableWidget, &QTableWidget::cellChanged, [this](int row, int column) {
         QString cellValue = tableWidget->item(row, column)->text();
-        saveAction("TableWidget cell changed: (" + QString::number(row) + ", " + QString::number(column) + ") " + cellValue);
+        saveAction("TableWidget cell changed: |" + QString::number(row) + "+" + QString::number(column) + "|" + cellValue);
     });
 
     connect(testSelector, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &MainWindow::updateTestLayout);
 
     connect(speedAction, &QAction::triggered, this, &MainWindow::changePlaybackSpeed);
+
+    updateTestLayout(0);
 
     setWidgetsEnabled(false);
 
@@ -180,6 +218,7 @@ void MainWindow::saveActionsToXml(const QString &filename)
 
 void MainWindow::replayActions(const QString &filename)
 {
+    clearFields();
     QFile file(filename);
     if (file.open(QIODevice::ReadOnly)) {
         QXmlStreamReader reader(&file);
@@ -193,11 +232,13 @@ void MainWindow::replayActions(const QString &filename)
                 QString action = reader.readElementText();
                 replayActions.append(action);
                 replayTimestamps.append(timestamp);
+
             }
         }
-
         file.close();
-
+        // Disable menu and test selector during replay
+        menu->setEnabled(false);
+        testSelector->setEnabled(false);
         int totalDuration = 0;
         for (int i = 0; i < replayActions.size(); ++i) {
             QDateTime currentTimestamp = QDateTime::fromString(replayTimestamps[i], Qt::ISODate);
@@ -205,38 +246,65 @@ void MainWindow::replayActions(const QString &filename)
             int duration = prevTimestamp.msecsTo(currentTimestamp) / playbackSpeed;
             totalDuration += duration;
 
-
             QTimer::singleShot(totalDuration, [=]() {
                 // Воспроизведение действия
-                if (replayActions[i].startsWith("Button clicked")) {
-                    button->animateClick();
-                } else if (replayActions[i].startsWith("LineEdit text changed:")) {
+                if (replayActions[i].startsWith("Button1 clicked")) {
+                    button1->setStyleSheet("background-color: green; color: white;");
+                }
+                else if (replayActions[i].startsWith("Button2 clicked")) {
+                    button2->setStyleSheet("background-color: red; color: white;");
+                }else if (replayActions[i].startsWith("LineEdit text changed:")) {
                     QString text = replayActions[i].mid(22).trimmed();
                     lineEdit->setText(text);
                 } else if (replayActions[i].startsWith("ComboBox index changed:")) {
                     QString indexStr = replayActions[i].mid(24).trimmed();
                     int index = indexStr.toInt();
                     comboBox->setCurrentIndex(index);
-                } else if (replayActions[i].startsWith("RadioButton toggled:")) {
+                } else if (replayActions[i].startsWith("RadioButton1 toggled:")) {
                     QString checkedStr = replayActions[i].mid(21).trimmed();
                     bool checked = (checkedStr == "1");
-                    radioButton->setChecked(checked);
+                    radioButton1->setChecked(checked);
+                } else if (replayActions[i].startsWith("RadioButton2 toggled:")) {
+                    QString checkedStr = replayActions[i].mid(21).trimmed();
+                    bool checked = (checkedStr == "1");
+                    radioButton2->setChecked(checked);
+                } else if (replayActions[i].startsWith("RadioButton3 toggled:")) {
+                    QString checkedStr = replayActions[i].mid(21).trimmed();
+                    bool checked = (checkedStr == "1");
+                    radioButton3->setChecked(checked);
                 } else if (replayActions[i].startsWith("TableWidget cell changed:")) {
+                    qDebug() << "table toggled";
                     QString cellStr = replayActions[i].mid(27).trimmed();
-                    QList<QString> cellCoords = cellStr.split(")")[0].split(", ");
+                    QList<QString> cellCoords = cellStr.split("|")[0].split("+");
 
                     if (cellCoords.size() == 2) {
                         int row = cellCoords[0].trimmed().toInt();
                         int column = cellCoords[1].trimmed().toInt();
-                        QString cellValue = cellStr.split(") ")[1];
+                        QString cellValue = cellStr.split("|")[1];
                         tableWidget->setItem(row, column, new QTableWidgetItem(cellValue));
                     }
+                    /*
+                    if (cellParts.size() == 2) {
+                        QString cellCoords = cellParts[0].trimmed();
+                        QString cellValue = cellParts[1].trimmed();
+                        QStringList coords = cellCoords.split("+");
+                        if (coords.size() == 2) {
+                            int row = coords[0].trimmed().toInt();
+                            int column = coords[1].trimmed().toInt();
+                            QTableWidgetItem *item = tableWidget->item(row, column);
+                            if (item) {
+                                item->setText(cellValue);
+                            }
+                        }
+                    }*/
                 }
             });
         }
         // Вызов диалогового окна после завершения воспроизведения
         QTimer::singleShot(totalDuration, [=]() {
             QMessageBox::information(this, "Воспроизведение завершено", "Воспроизведение записи завершено.");
+            menu->setEnabled(true);
+            testSelector->setEnabled(true);
         });
     }
 }
@@ -254,8 +322,26 @@ void MainWindow::clearFields()
 {
     lineEdit->clear();
     comboBox->setCurrentIndex(0);
-    radioButton->setChecked(false);
-    tableWidget->clearContents();
+    button1->setStyleSheet("background-color: white; color: green;");
+    button2->setStyleSheet("background-color: white; color: red;");
+    radioButton1->setAutoExclusive(false);
+    radioButton1->setChecked(false);
+    radioButton1->setAutoExclusive(true);
+    radioButton2->setAutoExclusive(false);
+    radioButton2->setChecked(false);
+    radioButton2->setAutoExclusive(true);
+    radioButton3->setAutoExclusive(false);
+    radioButton3->setChecked(false);
+    radioButton3->setAutoExclusive(true);
+//    tableWidget->clear();
+
+    // Clear answers from table widget
+    for (int i = 0; i < tableWidget->rowCount(); ++i) {
+        QTableWidgetItem *item = tableWidget->item(i, 1);
+        if (item) {
+            item->setText("");
+        }
+    }
 }
 
 void MainWindow::updateTestLayout(int index)
@@ -273,41 +359,186 @@ void MainWindow::updateTestLayout(int index)
             item->widget()->setParent(nullptr);
         }
         delete item;
-    }// Обновление расположения виджетов в соответствии с выбранным тестом
+    }
+
+    // Очистка выпадающего списка
+    comboBox->clear();
+
+    // Обновление расположения виджетов в соответствии с выбранным тестом
     switch (index) {
-    case 0:
+    case 0:{
         // Тест 1
-        layout->addWidget(button);
-        layout->addWidget(lineEdit);
+        comboBox->addItem("Математический анализ");
+        comboBox->addItem("Программирование");
+        comboBox->addItem("Линейная алгебра");
+        comboBox->addItem("Экономика");
+        layout->addWidget(new QLabel("Какой ваш любимый предмет?"));
         layout->addWidget(comboBox);
-        layout->addWidget(radioButton);
+
+        QWidget *buttonWidget1 = new QWidget;
+        QHBoxLayout *buttonLayout1 = new QHBoxLayout(buttonWidget1);
+        button1->setText("ДА");
+        button1->setStyleSheet("background-color: white; color: green;");
+        button2->setText("НЕТ");
+        button2->setStyleSheet("background-color: white; color: red;");
+        buttonLayout1->addWidget(button1);
+        buttonLayout1->addWidget(button2);
+        layout->addWidget(new QLabel("Вам нравится ходить на пары?"));
+        layout->addWidget(buttonWidget1);
+
+        layout->addWidget(new QLabel("Как зовут вашего любимого преподавателя?"));
+        layout->addWidget(lineEdit);
+
+        QWidget *radioWidget1 = new QWidget;
+        QHBoxLayout *radioLayout1 = new QHBoxLayout(radioWidget1);
+        layout->addWidget(new QLabel("Выберите как вы добираетесь до универа?"));
+        radioButton1->setText("Метро");
+        radioButton2->setText("Автобус");
+        radioButton3->setText("Автомобиль");
+        radioLayout1->addWidget(radioButton1);
+        radioLayout1->addWidget(radioButton2);
+        radioLayout1->addWidget(radioButton3);
+        layout->addWidget(radioWidget1);
+
+        layout->addWidget(new QLabel("Напишите, что вы думаете о каждом явлении."));
+        tableWidget->setHorizontalHeaderLabels(QStringList() << "Вопрос" << "Ответ");
+        tableWidget->verticalHeader()->hide();
+
+        QTableWidgetItem *item1_1 = new QTableWidgetItem("Как вам еда в столовой универа?");
+        item1_1->setFlags(Qt::ItemIsEditable);
+        tableWidget->setItem(0, 0, item1_1);
+
+        QTableWidgetItem *item2_1 = new QTableWidgetItem("Как вам состояние универа?");
+        item2_1->setFlags(Qt::ItemIsEditable);
+        tableWidget->setItem(1, 0, item2_1);
+
+        QTableWidgetItem *item3_1 = new QTableWidgetItem("Как вам качество материала программы?");
+        item3_1->setFlags(Qt::ItemIsEditable);
+        tableWidget->setItem(2, 0, item3_1);
+
         layout->addWidget(tableWidget);
         break;
-    case 1:
+    }
+    case 1:{
         // Тест 2
-        layout->addWidget(lineEdit);
-        layout->addWidget(button);
-        layout->addWidget(tableWidget);
+        comboBox->addItem("Блины");
+        comboBox->addItem("Омлет");
+        comboBox->addItem("Вафли");
+        comboBox->addItem("Каша");
+        layout->addWidget(new QLabel("Какой ваш любимый завтрак?"));
         layout->addWidget(comboBox);
-        layout->addWidget(radioButton);
+
+        QWidget *buttonWidget2 = new QWidget;
+        QHBoxLayout *buttonLayout2 = new QHBoxLayout(buttonWidget2);
+        button1->setText("ДА");
+        button1->setStyleSheet("background-color: white; color: green;");
+        button2->setText("НЕТ");
+        button2->setStyleSheet("background-color: white; color: red;");
+        buttonLayout2->addWidget(button1);
+        buttonLayout2->addWidget(button2);
+        layout->addWidget(new QLabel("Вы бы съели всю пиццу в одиночку?"));
+        layout->addWidget(buttonWidget2);
+
+        layout->addWidget(new QLabel("Сколько яиц вы добавляете в омлет?"));
+        layout->addWidget(lineEdit);
+
+        QWidget *radioWidget2 = new QWidget;
+        QHBoxLayout *radioLayout2 = new QHBoxLayout(radioWidget2);
+        layout->addWidget(new QLabel("Выбирите, что вы бы никогда не съели?"));
+        radioButton1->setText("Яблоко");
+        radioButton2->setText("Оливки");
+        radioButton3->setText("Чеснок");
+        radioLayout2->addWidget(radioButton1);
+        radioLayout2->addWidget(radioButton2);
+        radioLayout2->addWidget(radioButton3);
+        layout->addWidget(radioWidget2);
+
+        layout->addWidget(new QLabel("Напишите, что вы думаете о каждом блюде."));
+        tableWidget->setHorizontalHeaderLabels(QStringList() << "Вопрос" << "Ответ");
+        tableWidget->verticalHeader()->hide();
+
+        QTableWidgetItem *item1_2 = new QTableWidgetItem("Пицца");
+        item1_2->setFlags(Qt::ItemIsEditable);
+        tableWidget->setItem(0, 0, item1_2);
+
+        QTableWidgetItem *item2_2 = new QTableWidgetItem("Гребешок");
+        item2_2->setFlags(Qt::ItemIsEditable);
+        tableWidget->setItem(1, 0, item2_2);
+
+        QTableWidgetItem *item3_2 = new QTableWidgetItem("Холодец");
+        item3_2->setFlags(Qt::ItemIsEditable);
+        tableWidget->setItem(2, 0, item3_2);
+
+        layout->addWidget(tableWidget);
         break;
-    case 2:
+    }
+    case 2:{
         // Тест 3
-        layout->addWidget(radioButton);
-        layout->addWidget(tableWidget);
-        layout->addWidget(lineEdit);
+        comboBox->addItem("Сидеть дома");
+        comboBox->addItem("Гулять в парке");
+        comboBox->addItem("Сходить в кино");
+        comboBox->addItem("Посетить музей");
+        layout->addWidget(new QLabel("Какой ваш любимый досуг?"));
         layout->addWidget(comboBox);
-        layout->addWidget(button);
+
+        QWidget *buttonWidget3 = new QWidget;
+        QHBoxLayout *buttonLayout3 = new QHBoxLayout(buttonWidget3);
+        button1->setText("ДА");
+        button1->setStyleSheet("background-color: white; color: green;");
+        button2->setText("НЕТ");
+        button2->setStyleSheet("background-color: white; color: red;");
+        buttonLayout3->addWidget(button1);
+        buttonLayout3->addWidget(button2);
+        layout->addWidget(new QLabel("Вам нравится выходить на улицу?"));
+        layout->addWidget(buttonWidget3);
+
+        layout->addWidget(new QLabel("Какое место на Земле вы бы хотели посетить больше всего?"));
+        layout->addWidget(lineEdit);
+
+        QWidget *radioWidget3 = new QWidget;
+        QHBoxLayout *radioLayout3 = new QHBoxLayout(radioWidget3);
+        layout->addWidget(new QLabel("Выберите на что вы бы точно не тратили свое время."));
+        radioButton1->setText("Девушки");
+        radioButton2->setText("Учеба");
+        radioButton3->setText("Просмотр сериалов");
+        radioLayout3->addWidget(radioButton1);
+        radioLayout3->addWidget(radioButton2);
+        radioLayout3->addWidget(radioButton3);
+        layout->addWidget(radioWidget3);
+
+        layout->addWidget(new QLabel("Напишите, что вы думаете о каждом времяприпровождении."));
+        tableWidget->setHorizontalHeaderLabels(QStringList() << "Досуг" << "Ответ");
+        tableWidget->verticalHeader()->hide();
+
+        QTableWidgetItem *item1_3 = new QTableWidgetItem("Сходить в бар с друзьями");
+        item1_3->setFlags(Qt::ItemIsEditable);
+        tableWidget->setItem(0, 0, item1_3);
+
+        QTableWidgetItem *item2_3 = new QTableWidgetItem("Лежать на диване");
+        item2_3->setFlags(Qt::ItemIsEditable);
+        tableWidget->setItem(1, 0, item2_3);
+
+        QTableWidgetItem *item3_3 = new QTableWidgetItem("Ехать в метро");
+        item3_3->setFlags(Qt::ItemIsEditable);
+        tableWidget->setItem(2, 0, item3_3);
+
+        layout->addWidget(tableWidget);
         break;
+    }
     }
     setWidgetsEnabled(false);
 }
 
 void MainWindow::setWidgetsEnabled(bool enabled)
 {
-    button->setEnabled(enabled);
+    button1->setEnabled(enabled);
+    button2->setEnabled(enabled);
+
     lineEdit->setEnabled(enabled);
     comboBox->setEnabled(enabled);
-    radioButton->setEnabled(enabled);
+    radioButton1->setEnabled(enabled);
+    radioButton2->setEnabled(enabled);
+    radioButton3->setEnabled(enabled);
+
     tableWidget->setEnabled(enabled);
 }
