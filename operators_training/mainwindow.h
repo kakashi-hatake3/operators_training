bd302110845a23ed36b9cf20b5a9772d64db1215
arqwore <60177171+kakashi-hatake3@users.noreply.github.com>
2024-04-29 02:58:44 +0300

    added comments//

--- operators_training/mainwindow.h
+++ operators_training/mainwindow.h
@@ -4,56 +4,56 @@
 #include <QMainWindow>
 #include <QStringList>
 
-class QPushButton;
-class QLineEdit;
-class QComboBox;
-class QRadioButton;
-class QTableWidget;
-class QMenu;
-class QAction;
+class QPushButton;      // Объявление класса QPushButton для создания кнопок
+class QLineEdit;        // Объявление класса QLineEdit для создания поля ввода
+class QComboBox;        // Объявление класса QComboBox для создания комбо-бокса
+class QRadioButton;     // Объявление класса QRadioButton для создания переключателей
+class QTableWidget;     // Объявление класса QTableWidget для создания таблицы
+class QMenu;            // Объявление класса QMenu для создания меню
+class QAction;          // Объявление класса QAction для создания действий меню
 
-class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow  // Определение класса MainWindow, который является подклассом QMainWindow
 {
-    Q_OBJECT
+    Q_OBJECT  // Макрос Q_OBJECT для мета-объектной системы Qt
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);  // Конструктор класса
 
 private slots:
-    void startTraining();
-    void finishTraining();
-    void checkTrainingResult();
-    void saveAction(const QString &action);
-    void changePlaybackSpeed();
+    void startTraining();           // Слот для начала тренировки
+    void finishTraining();          // Слот для завершения тренировки
+    void checkTrainingResult();     // Слот для проверки результатов тренировки
+    void saveAction(const QString &action);  // Слот для сохранения действия
+    void changePlaybackSpeed();     // Слот для изменения скорости воспроизведения
 
 private:
-    QPushButton *button1;
-    QPushButton *button2;
-    QLineEdit *lineEdit;
-    QComboBox *comboBox;
-    QRadioButton *radioButton1;
-    QRadioButton *radioButton2;
-    QRadioButton *radioButton3;
-    QTableWidget *tableWidget;
-    QMenu *menu;
-    QAction *startAction;
-    QAction *exitAction;
-    QAction *finishAction;
-    QAction *checkAction;
-    QComboBox *testSelector;
-    QAction *speedAction;
+    QPushButton *button1;       // Указатель на кнопку 1
+    QPushButton *button2;       // Указатель на кнопку 2
+    QLineEdit *lineEdit;        // Указатель на поле ввода
+    QComboBox *comboBox;        // Указатель на комбо-бокс
+    QRadioButton *radioButton1; // Указатель на переключатель 1
+    QRadioButton *radioButton2; // Указатель на переключатель 2
+    QRadioButton *radioButton3; // Указатель на переключатель 3
+    QTableWidget *tableWidget;  // Указатель на таблицу
+    QMenu *menu;                // Указатель на меню
+    QAction *startAction;       // Указатель на действие начала тренировки
+    QAction *exitAction;        // Указатель на действие выхода
+    QAction *finishAction;      // Указатель на действие завершения тренировки
+    QAction *checkAction;       // Указатель на действие проверки результатов тренировки
+    QComboBox *testSelector;    // Указатель на комбо-бокс выбора теста
+    QAction *speedAction;       // Указатель на действие изменения скорости воспроизведения
 
-    QString currentUser;
-    QStringList actions;
-    QStringList timestamps;
+    QString currentUser;        // Текущий пользователь
+    QStringList actions;        // Список действий
+    QStringList timestamps;     // Список меток времени
 
-    void saveActionsToXml(const QString &filename);
-    void replayActions(const QString &filename);
-    void clearFields();
-    void updateTestLayout(int index);
-    void setWidgetsEnabled(bool isEnable);
+    void saveActionsToXml(const QString &filename);   // Метод для сохранения действий в XML-файл
+    void replayActions(const QString &filename);      // Метод для воспроизведения действий из XML-файла
+    void clearFields();                              // Метод для очистки полей
+    void updateTestLayout(int index);                // Метод для обновления расположения элементов теста
+    void setWidgetsEnabled(bool isEnable);           // Метод для установки активности элементов управления
 
-    double playbackSpeed = 1.0;
+    double playbackSpeed = 1.0;  // Скорость воспроизведения по умолчанию
 };
 
 #endif // MAINWINDOW_H
